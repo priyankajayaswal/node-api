e28f493a0bec827265f6791c4f32ab3409f769e7
priyanka.jayaswal <priyanka.jayaswal@CLPT1005.local>
2018-09-16 22:22:39 +0530

    updated urls of each website

--- api/adapters/webservices/base.drive.interface.js
+++ api/adapters/webservices/base.drive.interface.js
@@ -2,7 +2,6 @@ var required = function(){ throw new Error("Implement!"); };
 var BaseInterface = {
     formatData: required,
     convertToTemplate: required,
-    url: required
 };
 
 module.exports = BaseInterface;

39a48df05fa9843b58e5999fe032112c9d273c86
priyankajayaswal1 <priyankajayaswal025@gmail.com>
2018-11-16 13:13:05 +0530

    full data

--- api/utils/api.service.js
+++ api/utils/api.service.js
@@ -1,66 +1,33 @@
-const https = require('https');
+const reqPromise = require("request-promise")
 
-const makeApiCall = function makeApiCall(url, header, method, body={}) {
-    let options = {
-        url: "https://www.drivegaadi.com/search?pickupLocation=2&pickupDate=14-Sep-2018&pickupTime=21%3A30&dropOffDate=25-Sep-2018&dropOffTime=16%3A00",
-        method : "GET"
-    }
-
-    if (method == "POST") {
-        options.body = body
-    }
-    let responseObj = {count:0, message:""};
-    return new Promise((resolve, reject) => {
-        const req = https.request(options, function(response) {
-            console.log("response statusCode " +response.statusCode)
-            console.log("response headers " +response.headers)
-
-            response.on("data" , function(res) {
-                console.log("Data found  = "+response)
-                responseObj.message = response
-            });
-            
-        })
-
-        req.on("end" , function(err) {
-            responseObj.count = 1;
-            responseObj.message = err
-        });
-
-        req.end()
-        console.log("responseObjresponseObj"+ JSON.stringify(responseObj))
-        if (responseObj.count)
-        return reject(responseObj.message)
-        else 
-        return resolve(responseObj.message)
-
+var makeApiCall = function makeApiResponse(options){
+    return reqPromise(options)
+    .then(result => {
+        return Promise.resolve(parseJson(parseJson));
+    }).catch(err => {
+        return Promise.reject({error: JSON.stringify(err)});
     })
-    .then(data => console.log("DATA " + data))
-    .catch(err=> console.log("err "+ err))
+
 }
 
-const example = function() {
-    let options = {
-        url: "https://www.drivegaadi.com/search?pickupLocation=2&pickupDate=14-Sep-2018&pickupTime=21%3A30&dropOffDate=25-Sep-2018&dropOffTime=16%3A00",
-        method : "GET"
+var isJson = function isJson(obj) {
+    try {
+        let objParse = JSON.parse(obj)
+        return true
+    } catch(e) {
+        return false
     }
-    const req = https.request(options, (res) => {
-        console.log('statusCode:', res.statusCode);
-        console.log('headers:', res.headers);
-      
-        res.on('data', (d) => {
-          process.stdout.write(d);
-        });
-      });
-      
-      req.on('error', (e) => {
-        console.error(e);
-      });
-      req.end();
 }
-
+var parseJson = function parseJson(response) {
+    let res ;
+    if (response.body != undefined) {
+        res = isJson(response.body) ? JSON.parse(response.body) : response.body;
+    } else {
+        res = isJson(response) ? JSON.parse(response) : response;
+    }
+    return res
+}
 
 module.exports = {
-    makeApiCall,
-    example
-};
+    makeApiCall   
+}
